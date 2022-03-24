68b791ef9dafb85660eddb5ff4cbbea541913679
fujiindr <fujiindahrahayu@gmail.com>
2022-03-24 13:35:56 +0700

    API

--- app/Http/Controllers/API/BarangController.php
+++ app/Http/Controllers/API/BarangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Barang;
 use Illuminate\Http\Request;
 
 class BarangController extends Controller
@@ -14,7 +15,13 @@ class BarangController extends Controller
      */
     public function index()
     {
-        //
+        $barang = Barang::all();
+        return response()->json([
+            'status' => true,
+            'code' => 200,
+            'message' => 'berhasil',
+            'data' => $barang,
+        ]);
     }
 
     /**
@@ -35,7 +42,32 @@ class BarangController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $image = $request->cover;
+        $name = $image->getClientOriginalName();
+
+        $barang = new Barang;
+        $barang->id_kategori = $request->id_kategori;
+        $barang->nama_barang = $request->nama_barang;
+        // $barang->nama_kategori = $request->nama_kategori;
+        $barang->stok = $request->stok;
+        $barang->deskripsi = $request->deskripsi;
+        $barang->harga = $request->harga;
+        $barang->cover = $name;
+
+        // $image->move(public_path().' images/alat', $name);
+        if ($request->hasFile('cover')) {
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('images/barang/', $name);
+            $barang->cover = $name;
+        }
+        $barang->save();
+        return response()->json([
+            'status' => true,
+            'code' => 201,
+            'message' => 'Data Barang Berhasil Dibuat',
+            'data' => $barang,
+        ]);
     }
 
     /**
@@ -46,7 +78,23 @@ class BarangController extends Controller
      */
     public function show($id)
     {
-        //
+        $barang = Barang::findOrFail($id);
+        if ($barang) {
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => 'Show Data Barang',
+                'data' => $barang,
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 404,
+                'message' => 'Data Barang Tidak Ditemukan',
+                'data' => [],
+            ]);
+        }
     }
 
     /**
@@ -69,7 +117,45 @@ class BarangController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if ($barang){
+        $image = $request->cover;
+        $name = $image->getClientOriginalName();
+
+        $barang = new Barang;
+        $barang = Barang::findOrFail($id);
+        // $kategori = Kategori::all();
+        $barang->id_kategori = $request->id_kategori;
+        $barang->nama_barang = $request->nama_barang;
+        // $barang->nama_kategori = $request->nama_kategori;
+        $barang->stok = $request->stok;
+        $barang->deskripsi = $request->deskripsi;
+        $barang->harga = $request->harga;
+        $barang->cover = $name;
+
+        // $image->move(public_path().' images/alat', $name);
+        if ($request->hasFile('cover')) {
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('images/barang/', $name);
+            $barang->cover = $name;
+        }
+        $barang->save();
+        return response()->json([
+            'status' => true,
+            'code' => 201,
+            'message' => 'Data User Berhasil Dibuat',
+            'data' => $barang,
+        ]);
+        }
+        else {
+            return response()->json([
+                'status' => false,
+                'code' => 404,
+                'message' => 'Data User Tidak Ditemukan',
+                'data' => [],
+            ]);
+        }
+
     }
 
     /**
@@ -80,6 +166,23 @@ class BarangController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $barang = Barang::find($id);
+        if ($barang) {
+            $barang->delete();
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => 'Data barang Berhasil Di hapus',
+                'data' => $barang,
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 404,
+                'message' => 'Data barang Tidak Ditemukan',
+                'data' => [],
+            ]);
+        }
     }
 }
--- app/Http/Controllers/API/TransaksiController.php
+++ app/Http/Controllers/API/TransaksiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
 class TransaksiController extends Controller
@@ -14,7 +15,15 @@ class TransaksiController extends Controller
      */
     public function index()
     {
-        //
+        $transaksi = Transaksi::With('pembeli', 'barang')->get();
+        // $barang = Barang::all();
+        // $pembeli = Pembeli::all();
+        return response()->json([
+            'status' => true,
+            'code' => 200,
+            'message' => 'berhasil',
+            'data' => $transaksi,
+        ]);
     }
 
     /**
@@ -35,7 +44,42 @@ class TransaksiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $transaksi = new Transaksi;
+        $transaksi->id_pembeli = $request->id_pembeli;
+        $transaksi->id_barang = $request->id_barang;
+        $sisa_stok = Barang::findOrfail($request->id_barang);
+        if ($request->jumlah > $sisa_stok->stok) {
+            // Session::flash("flash_notification", [
+            //     "level" => "danger",
+            //     "massage" => "Stok Kurang",
+            // ]);
+            return redirect()->route('transaksi.index');
+        } elseif ($request->jumlah < 0) {
+            // Session::flash("flash_notification", [
+            //     "level" => "danger",
+            //     "massage" => "Jumlah Tidak Boleh Negatif",
+            // ]);
+            return redirect()->back();
+        } else {
+
+        }
+        $sisa_stok->stok -= $request->jumlah;
+        $sisa_stok->save();
+        $transaksi->jumlah = $request->jumlah;
+        // $transaksi->alamat = $request->alamat;
+        $transaksi->tanggal_beli = $request->tanggal_beli;
+        $price = Barang::findOrfail($request->id_barang);
+        // $transaksi->uang = $request->uang;
+        // $transaksi->kembali = $transaksi->uang - $transaksi->total;
+        $transaksi->harga = $price->harga;
+        $transaksi->total = $price->harga * $request->jumlah;
+        $transaksi->save();
+        return response()->json([
+            'status' => true,
+            'code' => 201,
+            'message' => 'Data Transaksi Berhasil Dibuat',
+            'data' => $transaksi,
+        ]);
     }
 
     /**
@@ -46,7 +90,23 @@ class TransaksiController extends Controller
      */
     public function show($id)
     {
-        //
+        $transaksi = Transaksi::findOrFail($id);
+        if ($transaksi) {
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => 'Show Data Transaksi',
+                'data' => $transaksi,
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 404,
+                'message' => 'Data Transaksi Tidak Ditemukan',
+                'data' => [],
+            ]);
+        }
     }
 
     /**
@@ -69,7 +129,54 @@ class TransaksiController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $transaksi = new Transaksi;
+        $transaksi = Transaksi::findOrfail($id);
+        if ($transaksi){
+        $transaksi->id_pembeli = $request->id_pembeli;
+        $transaksi->id_barang = $request->id_barang;
+        $sisa_stok = Barang::findOrfail($request->id_barang);
+        if ($request->jumlah > $sisa_stok->stok) {
+            // Session::flash("flash_notification", [
+            //     "level" => "danger",
+            //     "massage" => "Stok Kurang",
+            // ]);
+            return redirect()->route('transaksi.index');
+        } elseif ($request->jumlah < 0) {
+            // Session::flash("flash_notification", [
+            //     "level" => "danger",
+            //     "massage" => "Jumlah Tidak Boleh Negatif",
+            // ]);
+            return redirect()->back();
+        } else {
+
+        }
+
+        $sisa_stok->stok -= $request - l > jumlah;
+        $sisa_stok->save();
+        $transaksi->jumlah = $request->jumlah;
+        // $transaksi->alamat = $request->alamat;
+        $transaksi->tanggal_beli = $request->tanggal_beli;
+        $price = Barang::findOrfail($request->id_barang);
+        // $transaksi->uang = $request->uang;
+        // $transaksi->kembali = $transaksi->uang - $transaksi->total;
+        $transaksi->harga = $price->harga;
+        $transaksi->total = $price->harga * $request->jumlah;
+        $transaksi->save();
+            return response()->json([
+                'status' => true,
+                'code' => 201,
+                'message' => 'Data User Berhasil Dibuat',
+                'data' => $transaksi,
+            ]);
+        }
+        else {
+            return response()->json([
+                'status' => false,
+                'code' => 404,
+                'message' => 'Data User Tidak Ditemukan',
+                'data' => [],
+            ]);
+        }
     }
 
     /**
@@ -80,6 +187,23 @@ class TransaksiController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $transaksi = Transaksi::findOrFail($id);
+        if ($transaksi) {
+            $transaksi->delete();
+            return response()->json([
+                'status' => true,
+                'code' => 200,
+                'message' => 'Data Transaksi Berhasil Di hapus',
+                'data' => $transaksi,
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 404,
+                'message' => 'Data Trasaksi Tidak Ditemukan',
+                'data' => [],
+            ]);
+        }
     }
 }
